68156be53f1fdb4a991ca2ef5d1b19ce133c8715
HassanCoulibaly <hassansouleymanecoulibaly@gmail.com>
2025-03-01 17:26:25 +0000

    adding tasklist screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class TaskListScreen extends StatefulWidget {
+  const TaskListScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TaskListScreen> createState() => _TaskListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _TaskListScreenState extends State<TaskListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
